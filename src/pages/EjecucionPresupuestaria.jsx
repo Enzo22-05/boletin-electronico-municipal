c58c8bd8f47c65cd124496147dc1b3e1ccc0bc61
Enzo <enzobarrionuevo2205t@gmail.com>
2026-03-11 14:53:47 -0300

    legislacion y ejecucion presupuestaria

--- src/pages/EjecucionPresupuestaria.jsx
+++ src/pages/EjecucionPresupuestaria.jsx
@@ -1,12 +1,4 @@
+import DocList from "../components/DocList";
 export default function EjecucionPresupuestaria() {
-    return (
-      <div className="max-w-6xl mx-auto p-6">
-        <h1 className="text-2xl font-bold mb-4">
-          Ejecución Presupuestaria
-        </h1>
-        <p>
-          Aquí se publicarán los informes de ejecución presupuestaria municipal.
-        </p>
-      </div>
-    );
-  }
+  return <DocList tipo="EjecucionPresupuestaria" titulo="EjecucionPresupuestaria" />;
+}
